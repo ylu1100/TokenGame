df5a1bb0192b4858a6b6573656505e0fa49898c7
ylu1100 <43207435+ylu1100@users.noreply.github.com>
2018-12-21 08:43:27 -0500

    Update TokenPass.java

--- TokenGame/src/com/company/TokenPass.java
+++ TokenGame/src/com/company/TokenPass.java
@@ -6,23 +6,29 @@ public class TokenPass {
     public TokenPass(int playercount) {
         int[]board = new int [playercount];
         this.board=board;
-        this.CurrentPlayer = (int)(Math.random()*(playercount-1));
+        this.CurrentPlayer = (int)(Math.random()*(playercount));
         for(int i = 0;i<playercount;i++){
             board[i]=(int)((Math.random()*9)+1);
         }
     }
-    public void distributeCurrentPlayerTokens(){
-        for(int i = CurrentPlayer+1;i<board.length;i++){
-            board[i]++;
-            board[CurrentPlayer]--;
+    public void distributeCurrentPlayerTokens() {
+        for (int i = CurrentPlayer + 1; i < board.length; i++) {
+            if(board[CurrentPlayer]>0) {
+                board[i]++;
+                board[CurrentPlayer]--;
+            }
         }
-        if (board[CurrentPlayer]>0){
-            board[0]++;
-            board[CurrentPlayer]--;
+        if (board[CurrentPlayer] > 0) {
+            for (int i = 0; i<board.length; i++) {
+                if (board[CurrentPlayer] > 0) {
+                    board[i]++;
+                    board[CurrentPlayer]--;
+                }
+            }
         }
     }
     public int getCurrentPlayer(){
-        return CurrentPlayer;
+        return CurrentPlayer+1;
     }
     public int[] getBoard(){return board;
     }
